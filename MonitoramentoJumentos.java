46bee1fe966103016052dacb2d39256398f70b64
Guilherme Moura <guilhermemoura.silva2005@gmail.com>
2025-10-21 21:02:08 -0300

    MonitoramentoJumentos.java

---
+++ MonitoramentoJumentos.java
@@ -0,0 +1,42 @@
+//Guilherme de Moura Silva RA: 32521391
+public class MonitoramentoJumentos
+{
+    public static double calcularTaxaReducao = (int[] populacaoInicial[] -> 1400000; int populacaoAtual -> 84000){
+        double taxaReducao = (populacaoInicial - populacaoAtual);
+        return
+            double taxaReducao;
+        }
+        public static boolean estaEmRiscoCritico = (double taxaReducao) {
+            if (taxaReducao >= 90) {
+                return estaEmRiscoCritico (true);
+            } else {
+                return estaEmRiscoCritico (false);
+            }
+            public static int simularImpactoAbate = (int[] populacaoAtual -> 84000, int abatesAnuais -> 50000, int anosSimulados " "); {
+                int populacaoRestante = (populacaoAtual - abatesAnuais)/anosSimulados; {
+                return populacaoRestante;
+            }
+            public static void exibirDeclaracaoFinal (int populacaoRestante; int anos " ");
+            if (populacaoRestante <= 0) {
+                System.out.println("ALERTA");
+            } else { 
+                System.out.println("Ainda há esperança! Iniciativas de conservação são vitais.");
+            
+        }
+    }
+	public static void main(String[] args) {
+	    System.out.println("=== S.O.S. JUMENTO NORDESTINO - SISTEMA DE MONITORAMENTO ===");
+	    System.out.println("População inicial de referência: 1400000");
+	    System.out.println("População atual estimada: 84000");
+	    System.out.println("Abates anuais (simulados): 50000");
+	    System.out.println("[Análise Inicial] Taxa de Redução Histórica: 94,00%");
+	    System.out.println("[Status] Espécie em Risco CRÍTICO (Redução > 90%)!");
+	    Scanner scanner = new Scanner (System.in);
+	    System.out.println("Quantos anos futuros você deseja simular o impacto dos abates (Ex: 1, 3, 5)?");
+	    int = anosSimulados.nextline;
+	    System.out.println("--- RESULTADO DA SIMULAÇÃO ---");
+	    System.out.println("População restante após "anosSimulados "anos: "(populacaoRestante)"jumentos.");
+	    System.out.println(exibirDeclaracaoFinal);
+	    System.out.println("Programa de Monitoramento Finalizado.");
+	    
+}
